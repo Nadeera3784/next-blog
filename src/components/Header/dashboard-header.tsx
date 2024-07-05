1932c84173a6d451130f85691a65f5dcf47f9d2f
Nadeera Sampath <nadeera3784@gmail.com>
2024-07-05 14:32:55 +0530

    feat:dashboard navbar links updated

--- src/components/Header/dashboard-header.tsx
+++ src/components/Header/dashboard-header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function DashboardHeader() {
   return (
     <nav className="bg-white border-b border-gray-200">
@@ -9,19 +11,19 @@ export default function DashboardHeader() {
             </div>
             <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
               {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-              <a
-                href="#"
+              <Link
+                href="/dashboard"
                 className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 aria-current="page"
               >
                 Dashboard
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                href="/dashboard/categories"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
-                Team
-              </a>
+                Categories
+              </Link>
               <a
                 href="#"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
